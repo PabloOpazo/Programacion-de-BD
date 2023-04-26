c3b30bf913dffeacae8bbc6c2a51ac789ac239b2
Pablo Opazo <107130554+PabloOpazo@users.noreply.github.com>
2023-04-26 11:12:24 -0400

    Update ejercicio.sql

--- SEMANA 8/ejercicio.sql
+++ SEMANA 8/ejercicio.sql
@@ -1,53 +1,57 @@
-SET SERVEROUTPUT ON;
-
+SET SERVEROUTPUT ON; 
 DECLARE
-    -- Cursor de tarjetas
+    -- Almacena las tarjetas que seran procesadas
     CURSOR c_tarjetas IS
-        SELECT 
-            nro_tarjeta AS num_tarjeta,
-            TO_CHAR(fecha_solic_tarjeta, 'MM/YYYY') AS fecha,
-            numrun AS run
+        SELECT nro_tarjeta, 
+            TO_CHAR(fecha_solic_tarjeta, 'MM-YYYY') periodo,
+            numrun
         FROM tarjeta_cliente;
-        
-    -- Cursor transacciones
+    -- Almacena el nombre del cliente
+    nombre_cliente VARCHAR2(100);
+    -- Almacena las transacciones de una tarjeta
     CURSOR c_transacciones(p_nrotarjeta NUMBER) IS
-        SELECT
-            nro_transaccion AS nro_tranc,
-            TO_CHAR(fecha_transaccion, 'DD-MM-YYYY') AS fecha_tranc,
-            TO_CHAR(monto_transaccion, '$999g999g999') AS monto_tranc
+        SELECT nro_transaccion, fecha_transaccion, 
+            monto_transaccion
         FROM transaccion_tarjeta_cliente
         WHERE nro_tarjeta = p_nrotarjeta;
-    -- nombre cliente
-    nombre_cliente VARCHAR2(100);
-    monto_total NUMBER;
-    
+        --almacena la suma de todos los montos
+        suma_total NUMBER;
+        t_sin_movimientos NUMBER := 0;
+        t_con_movimientos NUMBER := 0;
 BEGIN
+    -- Recorrer el cursor
     FOR reg_tarjeta IN c_tarjetas LOOP
-    
-        DBMS_OUTPUT.PUT_LINE('NRO. TARJETA : '|| reg_tarjeta.num_tarjeta);
-        SELECT
-            appaterno||' '||apmaterno||' '||pnombre
+        -- Imprimir los datos de la tarjeta
+        DBMS_OUTPUT.PUT_LINE('NRO. TARJETA : ' || 
+            reg_tarjeta.nro_tarjeta);
+        -- Obtiene el nombre del cliente
+        SELECT appaterno || ' ' || apmaterno || ' ' || pnombre
         INTO nombre_cliente
         FROM cliente
-        WHERE reg_tarjeta.run = numrun;
-        DBMS_OUTPUT.PUT_LINE('CLIENTE : '|| nombre_cliente);
-        DBMS_OUTPUT.PUT_LINE('SOLICITADA EN : '|| reg_tarjeta.fecha);
-        
-        DBMS_OUTPUT.PUT_LINE('DETALLE MOVIMIENTOS ');
-        FOR reg_transaccion IN c_transacciones(reg_tarjeta.num_tarjeta) LOOP            
-        
-            IF reg_transaccion.monto_tranc > 0 THEN
-                DBMS_OUTPUT.PUT_LINE( reg_transaccion.nro_tranc||' '||reg_transaccion.fecha_tranc||' '||reg_transaccion.monto_tranc);
-                
-            ELSE
-                DBMS_OUTPUT.PUT_LINE('TARJETA SIN MOVIMIENTOS');
-            END IF;
-                
-            
+        WHERE numrun = reg_tarjeta.numrun;
+        DBMS_OUTPUT.PUT_LINE('CLIENTE : ' || nombre_cliente);
+        DBMS_OUTPUT.PUT_LINE('SOLICITADA EN : ' || reg_tarjeta.periodo);
+        DBMS_OUTPUT.PUT_LINE('--');
+        -- Recorrer las transacciones de la tarjeta
+        suma_total := 0;
+        DBMS_OUTPUT.PUT_LINE('DETALLE MOVIMIENTOS');
+        FOR reg_transaccion IN
+            c_transacciones(reg_tarjeta.nro_tarjeta) LOOP
+            DBMS_OUTPUT.PUT_LINE(reg_transaccion.nro_transaccion || ' '
+            || reg_transaccion.fecha_transaccion || ' ' ||
+            TO_CHAR(reg_transaccion.monto_transaccion,'$999g999g999'));
+            suma_total := suma_total + reg_transaccion.monto_transaccion;
         END LOOP;
-        CASE
-            DBMS_OUTPUT.PUT_LINE('MONTO TOTAL TRANSACCIONES : '||monto_total_tranc);
-        
+        IF suma_total = 0 THEN
+            DBMS_OUTPUT.PUT_LINE('TARJETA SIN MOVIMIENTOS');
+            t_sin_movimientos := t_sin_movimientos + 1;
+                
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('MONTO TOTAL TRANSACCIONES : '|| TO_CHAR(suma_total,'$999G999G999'));
+            t_con_movimientos := t_con_movimientos + 1;
+        END IF;
         DBMS_OUTPUT.PUT_LINE('------------------------------------------');
+        DBMS_OUTPUT.PUT_LINE('CANTIDAD DE TARJETAS CON MOVIMIENTOS : '||t_con_movimientos);
+        DBMS_OUTPUT.PUT_LINE('CANTIDAD DE TARJETAS SIN MOVIMIENTOS : '||t_sin_movimientos);
     END LOOP;
 END;
