dc822974d50ca55b0138112158b04f6a8d705186
Pablo Opazo <107130554+PabloOpazo@users.noreply.github.com>
2023-04-26 11:21:33 -0400

    Update and rename ejercicio.sql to ejercicio V1.sql

--- SEMANA 8/ejercicio V1.sql
+++ SEMANA 8/ejercicio V1.sql
@@ -22,8 +22,7 @@ BEGIN
     -- Recorrer el cursor
     FOR reg_tarjeta IN c_tarjetas LOOP
         -- Imprimir los datos de la tarjeta
-        DBMS_OUTPUT.PUT_LINE('NRO. TARJETA : ' || 
-            reg_tarjeta.nro_tarjeta);
+        DBMS_OUTPUT.PUT_LINE('NRO. TARJETA : ' || reg_tarjeta.nro_tarjeta);
         -- Obtiene el nombre del cliente
         SELECT appaterno || ' ' || apmaterno || ' ' || pnombre
         INTO nombre_cliente
@@ -37,21 +36,18 @@ BEGIN
         DBMS_OUTPUT.PUT_LINE('DETALLE MOVIMIENTOS');
         FOR reg_transaccion IN
             c_transacciones(reg_tarjeta.nro_tarjeta) LOOP
-            DBMS_OUTPUT.PUT_LINE(reg_transaccion.nro_transaccion || ' '
-            || reg_transaccion.fecha_transaccion || ' ' ||
-            TO_CHAR(reg_transaccion.monto_transaccion,'$999g999g999'));
+            DBMS_OUTPUT.PUT_LINE(reg_transaccion.nro_transaccion || ' '|| reg_transaccion.fecha_transaccion || ' ' ||TO_CHAR(reg_transaccion.monto_transaccion,'$999g999g999'));
             suma_total := suma_total + reg_transaccion.monto_transaccion;
         END LOOP;
         IF suma_total = 0 THEN
             DBMS_OUTPUT.PUT_LINE('TARJETA SIN MOVIMIENTOS');
             t_sin_movimientos := t_sin_movimientos + 1;
-                
         ELSE
             DBMS_OUTPUT.PUT_LINE('MONTO TOTAL TRANSACCIONES : '|| TO_CHAR(suma_total,'$999G999G999'));
             t_con_movimientos := t_con_movimientos + 1;
         END IF;
-        DBMS_OUTPUT.PUT_LINE('------------------------------------------');
-        DBMS_OUTPUT.PUT_LINE('CANTIDAD DE TARJETAS CON MOVIMIENTOS : '||t_con_movimientos);
-        DBMS_OUTPUT.PUT_LINE('CANTIDAD DE TARJETAS SIN MOVIMIENTOS : '||t_sin_movimientos);
+        DBMS_OUTPUT.PUT_LINE('------------------------------------------');        
     END LOOP;
+    DBMS_OUTPUT.PUT_LINE('CANTIDAD DE TARJETAS CON MOVIMIENTOS : '||t_con_movimientos);
+    DBMS_OUTPUT.PUT_LINE('CANTIDAD DE TARJETAS SIN MOVIMIENTOS : '||t_sin_movimientos);
 END;
